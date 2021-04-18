eb8a9c4f54b1c049321a572ace673f545de64a5e
William D. Jones <thor0505@comcast.net>
2021-04-17 22:31:12 -0400

    Remove unused register from USCI_A0_SPI_MODE- shouldn't have been generated.

--- src/usci_a0_spi_mode.rs
+++ src/usci_a0_spi_mode.rs
@@ -9,8 +9,7 @@ pub struct RegisterBlock {
     pub uca0br0: crate::Reg<uca0br0::UCA0BR0_SPEC>,
     #[doc = "0x03 - USCI A0 Baud Rate 1 Buffer"]
     pub uca0br1: crate::Reg<uca0br1::UCA0BR1_SPEC>,
-    #[doc = "0x04 - USCI A0 Modulation Control"]
-    pub uca0mctl: crate::Reg<uca0mctl::UCA0MCTL_SPEC>,
+    _reserved4: [u8; 1usize],
     #[doc = "0x05 - USCI A0 Status Register"]
     pub uca0stat: crate::Reg<uca0stat::UCA0STAT_SPEC>,
     #[doc = "0x06 - USCI A0 Receive Buffer"]
@@ -26,10 +25,6 @@ pub mod uca0ctl0;
 pub type UCA0CTL1 = crate::Reg<uca0ctl1::UCA0CTL1_SPEC>;
 #[doc = "USCI A0 Control Register 1"]
 pub mod uca0ctl1;
-#[doc = "UCA0MCTL register accessor: an alias for `Reg<UCA0MCTL_SPEC>`"]
-pub type UCA0MCTL = crate::Reg<uca0mctl::UCA0MCTL_SPEC>;
-#[doc = "USCI A0 Modulation Control"]
-pub mod uca0mctl;
 #[doc = "UCA0STAT register accessor: an alias for `Reg<UCA0STAT_SPEC>`"]
 pub type UCA0STAT = crate::Reg<uca0stat::UCA0STAT_SPEC>;
 #[doc = "USCI A0 Status Register"]

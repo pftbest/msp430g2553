642f9002637e71449585adf8e3a5feb439c3befe
William D. Jones <thor0505@comcast.net>
2022-01-25 17:30:08 -0500

    WDTIS is safe to write.

--- src/watchdog_timer/wdtctl.rs
+++ src/watchdog_timer/wdtctl.rs
@@ -361,7 +361,7 @@ pub struct WDTIS_W<'a> {
 impl<'a> WDTIS_W<'a> {
     #[doc = r"Writes raw bits to the field"]
     #[inline(always)]
-    pub unsafe fn bits(self, value: u8) -> &'a mut W {
+    pub fn bits(self, value: u8) -> &'a mut W {
         self.w.bits = (self.w.bits & !0x03) | (value as u16 & 0x03);
         self.w
     }
